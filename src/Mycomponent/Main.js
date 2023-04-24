15828b68d9b8ddce31ee0647947b66da9d8ae171
Deepak Thapliyal <112935088+Thisis2pac@users.noreply.github.com>
2023-04-24 09:11:07 +0530

    initialling repo

--- src/Mycomponent/Main.js
+++ src/Mycomponent/Main.js
@@ -1,84 +1,94 @@
 import React, { useState } from "react";
 
 const Mycalcu = (props) => {
+  // Define state for the input value
   const [input, setInput] = useState("");
+
+  // Handle input change
   const handleOnchange = (e) => {
     setInput(e.target.value);
   }
 
+  // Handle backspace button
   const handleBackspace = () => {
     setInput(input.slice(0, -1));
   }
+
+  // Handle reset button
   const handlereset = () => {
     setInput("")
   }
+
+  // Handle calculation
   const handleCalculate = () => {
     try {
       setInput(eval(input).toString());
-
     } catch (error) {
       alert("please Enter currect number " + error)
-
     }
   }
-  //Handle sitution if user did not include something after decimal 
+
+  // Handle decimal button
   const handleDecimal = () => {
     if (input.indexOf(".") === -1) {
       setInput(input + ".");
     }
-
   }
-  //Set dark mode turn on and off by just changing some colors 
+
+  // Define state for dark mode
   const [isdarkmodeon, isdarkmodeof] = useState(false);
+
+  // Handle dark mode button
   const handledarkmode = () => {
     console.log("darkmode")
     isdarkmodeof(!isdarkmodeon)
   }
 
-return (
-  
+  return (
+
     <div className={`calculator ${isdarkmodeon ? 'dark-mode' : ""}`}>
       <div className="display">
 
-        <input type="text"  className={`input ${isdarkmodeon ? 'dark-mode2' : ""}`} value={input} onChange={handleOnchange}  />
+        <input type="text" className={`input ${isdarkmodeon ? 'dark-mode2' : ""}`} value={input} onChange={handleOnchange} />
 
       </div>
       <div className="grid-container" >
 
-        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={()=>{handledarkmode(); props.onButtonClick()} }  > <span class="material-symbols-outlined" >
-          {isdarkmodeon? "dark_mode": "light_mode"}
+        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { handledarkmode(); props.onButtonClick() }}  > <span class="material-symbols-outlined" >
+          {isdarkmodeon ? "dark_mode" : "light_mode"}
         </span></button>
-        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={()=>{ props.onButtonClick()} } ><span class="material-symbols-outlined">
+        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { props.onButtonClick() }} ><span class="material-symbols-outlined">
           percent
         </span></button>
 
-        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={()=>{handleBackspace(); props.onButtonClick()} }><span className="material-symbols-outlined">
+        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { handleBackspace(); props.onButtonClick() }}><span className="material-symbols-outlined">
           backspace
         </span></button>
         <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => {
-          setInput(input + "/") ;props.onButtonClick()  ; props.onButtonClick();
+          setInput(input + "/"); props.onButtonClick(); props.onButtonClick();
 
         }}>/</button>
-        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "7") ; props.onButtonClick(); }}>7</button>
-        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "8") ; props.onButtonClick(); ;
-       }}>8</button>
-        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "9") ; props.onButtonClick(); }}>9</button>
-        <button className={` black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { setInput(input + "*") ; props.onButtonClick(); }}>*</button>
-        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "4") ; props.onButtonClick(); }}>4</button>
-        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "5") ; props.onButtonClick(); }}>5</button>
-        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "6") ; props.onButtonClick(); }}>6</button>
-        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { setInput(input + "-") ; props.onButtonClick(); }}>-</button>
-        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "1") ; props.onButtonClick(); }}>1</button>
-        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "2") ; props.onButtonClick(); }}>2</button>
-        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "3") ; props.onButtonClick(); }}>3</button>
-        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { setInput(input + "+") ;props.onButtonClick(); }}>+</button>
-        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`}  onClick={() => { setInput(input + "0") ; props.onButtonClick(); }}>0</button>
-        <button className={`b ${isdarkmodeon ? 'for-grey' : ""}`} onClick={()=>{handleDecimal(); props.onButtonClick()} } >.</button>
-        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={()=>{handlereset(); props.onButtonClick()} } ><span class="material-symbols-outlined">
+        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "7"); props.onButtonClick(); }}>7</button>
+        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => {
+          setInput(input + "8"); props.onButtonClick();;
+        }}>8</button>
+        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "9"); props.onButtonClick(); }}>9</button>
+        <button className={` black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { setInput(input + "*"); props.onButtonClick(); }}>*</button>
+        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "4"); props.onButtonClick(); }}>4</button>
+        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "5"); props.onButtonClick(); }}>5</button>
+        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "6"); props.onButtonClick(); }}>6</button>
+        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { setInput(input + "-"); props.onButtonClick(); }}>-</button>
+        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "1"); props.onButtonClick(); }}>1</button>
+        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "2"); props.onButtonClick(); }}>2</button>
+        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "3"); props.onButtonClick(); }}>3</button>
+        <button className={`black ${isdarkmodeon ? 'for-black' : ""}`} onClick={() => { setInput(input + "+"); props.onButtonClick(); }}>+</button>
+        <button className={` ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { setInput(input + "0"); props.onButtonClick(); }}>0</button>
+        <button className={`b ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { handleDecimal(); props.onButtonClick() }} >.</button>
+        <button className={`  ${isdarkmodeon ? 'for-grey' : ""}`} onClick={() => { handlereset(); props.onButtonClick() }} ><span class="material-symbols-outlined">
           refresh
         </span></button>
 
-        <button className={`enter  ${isdarkmodeon ? 'for-red' : ""}`}  onClick={()=>{handleCalculate(); props.onButtonClick()} } >=</button>
+        <button className={`enter  ${isdarkmodeon ? 'for-red' : ""}`} onClick={() => { handleCalculate(); props.onButtonClick() }} > =</button>
       </div>
     </div>
   );
